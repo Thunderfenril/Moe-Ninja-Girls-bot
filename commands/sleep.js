a6137b8084b4abd3eb3ed503afee361b85e31db4
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-12 21:55:59 +0200

    Changed setName and setDescription, created problem for deploy-commands

--- commands/sleep.js
+++ commands/sleep.js
@@ -3,8 +3,8 @@ const botownerid=221352736657113088;
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('sleep')
+		.setDescription('Command to put the bot to sleep'),
 	async execute(interaction) {
 		if (interaction.member.id==botownerid){
             interaction.client.channels.cache.get('697617573394776114').send('Good night everybody');
