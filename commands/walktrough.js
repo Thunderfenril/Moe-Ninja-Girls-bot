1170848cd57eb7bd2b81127eec2a64d0c8a4bc11
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-13 10:42:09 +0200

    Added option, need to write the code

--- commands/walktrough.js
+++ commands/walktrough.js
@@ -3,7 +3,11 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('walktrough')
-		.setDescription('Walktrough command'),
+		.setDescription('Walktrough command')
+		.addNumberOption(option =>
+			option.setName("chapter")
+			.setDescription("The number of the season you want the walkthrough")
+			.setRequired(true)),
 	async execute(interaction) {
 		return interaction.reply('Pong!');
 	},
