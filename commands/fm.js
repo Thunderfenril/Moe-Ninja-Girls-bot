d378b9b95fca370c9d09a18810ac638f44592126
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-19 10:46:49 +0200

    Started the fm update, option added, core need to be reworked

--- commands/fm.js
+++ commands/fm.js
@@ -4,7 +4,48 @@ const fmdata = require("../array/datafm");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('fm')
-		.setDescription('The fight master command'),
+		.setDescription('The fight master command')
+		.setIntegerOption(option =>
+			option.setName("stage")
+			.setDescription("Select the stage. Final is 10")
+			.setRequired(true)
+			.addChoice(1, "Stage 1")
+			.addChoice(2, "Stage 2")
+			.addChoice(3, "Stage 3")
+			.addChoice(4, "Stage 4")
+			.addChoice(5, "Stage 5")
+			.addChoice(6, "Stage 6")
+			.addChoice(7, "Stage 7")
+			.addChoice(8, "Stage 8")
+			.addChoice(9, "Stage 9")
+			.addChoice(10, "Final stage"))
+		.setStringOption(option =>
+			option.setName("weakness")
+			.setDescription("The weakness of the stage")
+			.setRequired(true)
+			.addChoice("fire")
+			.addChoice("ice")
+			.addChoice("thunder"))
+		.setIntegerOption(option =>
+			option.setName("fire")
+			.setDescription("The value of your fire power")
+			.setRequired(true))
+		.setIntegerOption(option =>
+			option.setName("ice")
+			.setDescription("The value of your ice power")
+			.setRequired(true))
+		.setIntegerOption(option =>
+			option.setName("thunder")
+			.setDescription("The value of your thunder power")
+			.setRequired(true))
+		.setIntegerOption(option =>
+			option.setName("shuriken")
+			.setDescription("The number of shuriken you own")
+			.setRequired(true))
+		.setIntegerOption(option =>
+			option.setName("level")
+			.setDescription("The current level of your account")
+			.setRequired(true)),
 	async execute(interaction) {
 		//Creation of the variable
 		let valueStage;
@@ -129,7 +170,7 @@ module.exports = {
 				  })
 				})
 			  } else {
-				message.reply("This weakness doesn't exist")
+				message.reply("This weakness doesn't exist");
 			  }
 			})
 		  } else {
