8e72be84fde9047c9fca8166de445c6a9cacc875
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-19 21:14:22 +0200

    Miscellaneous change

--- commands/profile.js
+++ commands/profile.js
@@ -32,14 +32,10 @@ module.exports = {
         .setFooter(datainfo[indexPosition][0]+" "+datainfo[indexPosition][1]+" profile page")
         .setThumbnail(datainfo[indexPosition][13])
         .setImage(datainfo[indexPosition][13])
-        .addFields({name: "Unique gift:",
-                    value: datainfo[indexPosition][6][0]})
-        .addFields({name: "Normal gifts:",
-                    value: datainfo[indexPosition][6][1]})
-        .addFields({name:"\u200B",
-                    value:"\u200B"})
-        .addFields({name:"Trivia",
-                    value:"\nSeyuu: "+datainfo[indexPosition][7]+"\nThree sizes: "+datainfo[indexPosition][5][0]+"/"+datainfo[indexPosition][5][1]+"/"+datainfo[indexPosition][5][2]+"\nBust Size: "+datainfo[indexPosition][5][3]+"\nBirthday: "+datainfo[indexPosition][2][0]+" "+monthString+"\nZodiac sign: "+datainfo[indexPosition][3]+"\nEye color: "+datainfo[indexPosition][8]+"\nHair color: "+datainfo[indexPosition][9]+"\nHobbies: "+datainfo[indexPosition][10]+"\nLikes: "+datainfo[indexPosition][11]})
+        .addFields({name: "Unique gift:", value: datainfo[indexPosition][6][0]})
+        .addFields({name: "Normal gifts:", value: datainfo[indexPosition][6][1]})
+        .addFields({name:"\u200B", value:"\u200B"})
+        .addFields({name:"Trivia", value:"\nSeyuu: "+datainfo[indexPosition][7]+"\nThree sizes: "+datainfo[indexPosition][5][0]+"/"+datainfo[indexPosition][5][1]+"/"+datainfo[indexPosition][5][2]+"\nBust Size: "+datainfo[indexPosition][5][3]+"\nBirthday: "+datainfo[indexPosition][2][0]+" "+monthString+"\nZodiac sign: "+datainfo[indexPosition][3]+"\nEye color: "+datainfo[indexPosition][8]+"\nHair color: "+datainfo[indexPosition][9]+"\nHobbies: "+datainfo[indexPosition][10]+"\nLikes: "+datainfo[indexPosition][11]})
       return interaction.reply({ embeds: [embedTest]})
     }
 	},
