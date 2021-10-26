1f6799fe3d742ccb114ae03126505c9c7db3a31a
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-26 17:52:16 +0200

    Made the same change than zap

--- commands/whip.js
+++ commands/whip.js
@@ -1,27 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const wait = require('util').promisify(setTimeout);
 
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('To whip something'),
+		.setDescription('To whip something')
+    .addUserOption(option =>
+      option.setName("target")
+      .setDescription("The one you wish to zap")
+      .setRequired(false))
+    .addStringOption(option =>
+      option.setName("text")
+      .setDescription("Some text you want, I don't know I'm a description")
+      .setRequired(false)),
 	async execute(interaction) {
-		const taggedUser = message.mentions.users.first(); //We get the first mention
-            if (taggedUser!==undefined) { //If taggedUser is not undefined...
+		const taggedUser = interaction.options.getUser('target'); //We get the mention
+    const stringOption = interaction.options.getString('text');
+            if (taggedUser!==null) { //If taggedUser is not undefined...
         if (taggedUser.id === interaction.member.id) { //If the author of the message tagged himself...
-          return interaction.reply("<@"+interaction.member.id+"> is a masochistic pervert."); //We send this message
-          return interaction.reply('<a:RanExposed:745341250852356186>')
+          interaction.reply("<@"+interaction.member.id+"> is a masochistic pervert."); //We send this message
+          return interaction.followUp('<a:RanExposed:745341250852356186>')
         } else if(taggedUser.id === '758252653435944970' ||taggedUser.id === '720943363679059979') { //If he tag the bot or another bot then...
-          return interaction.reply("*Focus*").then((msg) => { //We send a message, and we will..
-            setTimeout(function() { msg.edit("Mega whip on <@"+interaction.member.id+">"); //edit the message...
-          }, 1000) //after 1s
-          })
+          await interaction.reply("*Focus*")
+          await wait(1000);
+          await intereaction.editReply("Mega whip on <@"+interaction.member.id+">")
+          return;
         } else {
           interaction.reply("Whip at <@"+taggedUser+">")
-          return interaction.reply("<a:LMyuWhip1:676408951537139732><a:LMyuWhip2:676409245263986731>")
+          return interaction.followUp("<a:LMyuWhip1:676408951537139732><a:LMyuWhip2:676409245263986731>")
         }
-        } else if(taggedUser === undefined){
-        return interaction.reply("Whip at "+args.join(' '))
+        } else if(null === taggedUser && null !== stringOption){
+        return interaction.reply("Whip at "+stringOption)
         } else {
         return interaction.reply("Whip on everybody")
         } 
