d60227af52ca84050afe58b46a9c4e83af1da82c
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-26 17:37:22 +0200

    Testing the command

--- commands/zap.js
+++ commands/zap.js
@@ -4,23 +4,31 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('zap')
-		.setDescription('To zap something'),
+		.setDescription('To zap something')
+    .addUserOption(option =>
+      option.setName("target")
+      .setDescription("The one you wish to zap")
+      .setRequired(false))
+    .addStringOption(option =>
+      option.setName("text")
+      .setDescription("Some text you want, I don't know I'm a description")
+      .setRequired(false)),
 	async execute(interaction) {
-		const taggedUser = message.mentions.users.first(); //We get the first mention
-        if (taggedUser!==undefined) { //If taggedUser is not undefined...
+		const taggedUser = interaction.options.getUser('target'); //We get the mention
+    const stringOption = interaction.options.getString('text');
+        if (taggedUser!==null) { //If taggedUser is not undefined...
           if (taggedUser.id === interaction.member.id) { //If the author of the message tagged himself...
-            return interaction.reply("<@"+interaction.member.id+"> is a masochistic Hentai."); //We send this message
-            return interaction.reply('<a:EnjuZap:653780228933681192>')
+            interaction.reply("<@"+interaction.member.id+"> is a masochistic Hentai."); //We send this message
+            return interaction.followUp('<a:EnjuZap:653780228933681192>')
           } else if(taggedUser.id === '758252653435944970' ||taggedUser.id === '720943363679059979') { //If he tag the bot or another bot then...
-            return interaction.reply("*Focus*").then((msg) => { //We send a message, and we will..
-              setTimeout(function() { msg.edit("Mega zap on <@"+interaction.member.id+">"); //edit the message...
-            }, 1000) //after 1s
-            })
+            interaction.reply("*Focus*");
+            wait(1000);
+            interaction.editReply("Mega zap on <@"+interaction.member.id+">");
           } else {
             return interaction.reply("Zap at <@"+taggedUser+">")
           }
         } else if(taggedUser === undefined){
-          return interaction.reply("Zap at "+args.join(' '))
+          return interaction.reply("Zap at "+stringOption.join(' '))
         } else {
           return interaction.reply("Zap on everybody")
         }
