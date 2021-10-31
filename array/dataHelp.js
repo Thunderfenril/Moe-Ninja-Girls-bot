bd0da05f8c94ecad7f3a85e96744cd5649dbd64d
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-31 20:03:57 +0100

    Updated to work with the / commands

--- array/dataHelp.js
+++ array/dataHelp.js
@@ -1,43 +1,44 @@
 module.exports = [ //Array for the help command
 
-    /*[ , , ["\u200b", "\u200b"], [, "\u200b"], ["Arguments:", ], ["Explanation:", ], ["Example of use:", ]]*/ //Format of the array
+    /*[Name of the command , Small description, ["\u200b", "\u200b"], [Format, "\u200b"], ["Arguments:", Explanation of the argument], ["Explanation:", Full explanation], ["Example of use:", Example]]*/ //Format of the array
   
-    ["The changelog command.", "Let the know the latest update on the bot", ["\u200B", "\u200B"], ["changelog", "\u200B"], ["Arguments", "No argument are needed"], ["Explanation", "Send an embed to inform the user from the latest update on the bot"], ["Example of use", "changelog"]],
-  
-    ["The gacha command.", "Let us hear the holy prayer", ["\u200B", "\u200B"], ["gacha", "\u200B"], ["Arguments", "No argument are needed"], ["Explanation:", "After the command is called, the bot will send the holy prayer of the gacha genre."], ["Example of use:", "gacha"]],
-  
-    ["The hug command.", "No one will be able to hug them, not even the author of the bot.", ["\u200B", "\u200B"], ["hug", "\u200b"], ["Arguments:", "No arguments are needed"], ["Explanation:", "After you launch the command, you will see the bot reacting to your tentative of huging her."], ["Example of use:", "hug"]],
-  
-    ["The congrats command" , "Let's congratulate someone from it's pull", ["\u200B", "\u200B" ], ["congrats @someone", "\u200B"], ["Arguments:", "You need to mention someone"], ["Explanation:", "The bot will send a message to congratulate the person you mentionned, and a few second later, the message will be edited"], ["Example of use:", "congrats @thunderfenril#4235"]],
-  
-    ["The roll command" , "The gacha simulator for the RPG", ["\u200b", "\u200b"], ["roll toolsbody 110", "\u200b"], ["Arguments:", "Two arguments are needed. The first one is the category of the pull, while the second one determine if you do a single or multi pull."], ["Explanation:", "After you choose launched the command with the correct arguments, the bot will generate a random number and look for the corresponding rarity of the category you chose.\nAfter it will create a new random number, to look for the item.\nFor the multipull, it will do it 10 times instead of once."], ["Example of use:", "roll tools 10"]],
-  
-    ["The profile command" , "It will show a small embed with the description  of the girl.", ["\u200b", "\u200b"], ["profile nameOfTheGirl", "\u200b"], ["Arguments:", "One argument is needed, the name of the girl"], ["Explanation:", "The bot will recognize the name, if right spelled, and will generate an embed that will give what the girl like, their Seyuu, hobby, etc."], ["Example of use:", "profile enju"]],
-  
-    ["The data command.", "Give some information on the height and 3 sizes.", ["\u200b", "\u200b"], ["data [height bust waist hips]", "\u200b"], ["Arguments:", "It needs 1 or 0 argument."], ["Explanation:","If an argument is given, it will return an embed, with the ranking of the girls based on the chosen category.\nIf no argument is given, it will give a general ranking of each category, with the 3 top of each category."], ["Example of use:", "data height"]],
-  
-    ["The ranking command" ,"Give some information about a girl on their rank in several category." , ["\u200b", "\u200b"], ["ranking nameOfAGirl", "\u200b"], ["Arguments:", "One argument is needed"], ["Explanation:", "After the command is launched, the bot will generate an embed which will give information on the ranking of the girl based on common trait with the other, and with personal characteristics."], ["Example of use:", "ranking Ran"]],
-  
-    ["The suggestion/report command" ,"If you want to report something or doing an suggestion anonymously." , ["\u200b", "\u200b"], ["dm testHere", "\u200b"], ["Arguments:", "1 is needed"], ["Explanation:", "In order to do this, you have to go in DM with the bot and do the command, before sending the command, write the text.\nWhen you send the message, the bot will send the message to the bot author, without saying who sent the message."], ["Example of use:", "dm Someone in mng-general broke the rule [insert random rule number.]"]],
-  
-    ["The whip/zap command" , "Do you want to whip or zap someone ?", ["\u200b", "\u200b"], ["whip @mention someone or zap @mention someone", "\u200b"], ["Arguments:", "You have to mention someone."], ["Explanation:", "This very old command is back. You will be able to whip or zap someone again, there will be of course a special message if you try to whip or zap yourself."], ["Example of use:", "whip @Bluedragon#7657"]],
-  
-    ["The birthday command" ,"To know who has it's birthday next" , ["\u200b", "\u200b"], ["birthday", "\u200b"], ["Arguments:", "No argument is needed"], ["Explanation:", "It will say which girl in the mng-verse will have it's birthday next, and in how many days."], ["Example of use:", "birthday"]],
-  
-    ["The ninjutsu command" ,"The command to try to look like a ninja" , ["\u200b", "\u200b"], ["ninjutsu target", "\u200b"], ["Arguments:", "One argument is needed, eitehr a mention or a text."], ["Explanation:", "You will use a random ninjutsu against the target, but because you don't know how to use it, you only have a 50% to hit the target, else you will hit yourself."], ["Example of use:", "ninjutsu @Bluedragon#7657"]],
-  
-    ["The fight master command." , "aka the annoying one to make.", ["\u200b", "\u200b"], ["fm", "\u200b"], ["Arguments:", "None"], ["Explanation:", "The bot will ask you a series of question, to which you will have to answer. Depending of your result, the bot will tell you if you can beat the stage and in how many rounds."], ["Example of use:", "fm"]],
-  
-    ["The versus command" ,"To compare 2 characters" , ["\u200b", "\u200b"], ["versus name1 name 2", "\u200b"], ["Arguments:", "Two different names are needed, some characters can't be used cause we don't have all the data *curse SOLMARE for being slow with it*"], ["Explanation:", "It will compare the 2 characters based on their Height, Bust, Waist, Hips, Strength, Shinobi/Spiritual power, Intelligence and Agility.\nIf one has a bigger number, then she get +1 to her score, in case of tie, nobody get a point."], ["Example of use:", "versus Enju Venus"]],
+    ["The birthday command", "Allow to get know info on the birthday", ["\u200b", "\u200b"], ["birthday [name]", "\u200B"], ["Argument:", "The argument isn't necessary, but you can put the name of one of the girl in it"], ["Explanation:", "Create a small sentence, if no argument is put, will says when is the next birthday, and if it's the actual day, will make a small special sentence; in case a name is given, will says how much days are left before we reach the birthday."], ["Example:", "birthday Enju"]],
 
-    ["github" , "The command to direct to the github of the bot", ["\u200b", "\u200b"], ["github", "\u200b"], ["Arguments:", ], ["Explanation:", ], ["Example of use:", ]],
-  
-    ["You called for some help ?" ,"Here are the command." , ["__Command about MNG:__", "data: Give info and ranking on the different sizes.\nranking: Give the ranking of the girl.\nprofile: Give you some basic info on the girl.\nfm: The command about to know if you can beat  a certain stage.\nversus: To compare 2 characters.\nbirthday: Who is the next one to get her birthday ?"], ["__Fun command__", "gacha: Let us hear the holy prayer.\nhug: Good luck.\ncongrats ``mention``: Congratulation to ``mention`` for his/her luck.\nroll: Let the salt come.\nninjutsu: You use a random ninjutsu on someone.\nrevive: To ressurect someone.\nwhip: To whip someone.\nzap: To zap someone.\n"], ["__Info__", "changelog: To be up to date with the change.\nhelp [command name]: Do I have to explain ?\ndm: To send an anonymous message to the author of the bot **DM only**\nThis is currently the version 4.3.0 of the bot."], ["__Suggestion__", "In order to make a suggestion, request, fix, send your message under this format:\n@thunderfenril#4235\n[ ]....\n\nHere is an example:\n```@thunderfenril#4235\n[request] Let me hug the bot.```\n*for info, I will never fulfill this request*\nYou can also use dm command to do one."]]
+    ["The changelog command", "The command to get the news on the bot", ["\u200b", "\u200b"], ["changelog", "\u200b"], ["Arguments:", "There is no argument needed"], ["Explanation:", "This will create an embed with the news on the bot and the main things planned for the bot"], ["Example:", "changelog"]],
+
+    ["The congrats command", "Allow you to congratulate someone", ["\u200b", "\u200b"], ["congrats @someone", "\u200b"], ["Argument:", "You need to mention someone"], ["Explanation:", "By mentionning someone, it will create a random number, if the number is even, will send the message 1, else the message 2."], ["Example:", "congrats @someone"]],
+
+    ["The data command", "Allow you to get data on a ranking based on a category", ["\u200b", "\u200b"], ["data [category]", "\u200b"], ["Argument:", "You can choose one in the list, or just let it be blank"], ["Explanation:", "Will create an embed.\nIf no  category is given, will create an embed, and give the top 3 in each category.\nIf a category is given, will give the ranking of all the girl based on this category."], ["Example:", "data height"]],
+
+    ["The fight master command", "If you want to know if you can beat the stage", ["\u200b", "\u200b"], ["fm stage weakness fire ice thunder level shuriken", "\u200b"], ["Arguments:", "You will have to choose which stage in a list, then the weakness in a list, then you will have to input the value of each other category as a number"], ["Explanation:", "By filling the arguments, it will look the HP of the stage, then will calculate your power with and without juice.\nIt will tell you if you can beat the stage, and in how many rounds."], ["Example:", "fm 1 thunder 0 0 804 90 500"]],
+
+    ["The gacha command", "Let's hear the holy prayer", ["\u200b", "\u200b"], ["gacha", "\u200b"], ["Argument:", "There is no argument"], ["Explanation:", "Send to us the holy prayer"], ["Example:", "gacha"]],
+
+    ["The github command", "A direct link to the project", ["\u200b", "\u200b"], ["github", "\u200b"], ["Argument:", "There is no argument"], ["Explanation:", "Send a link to the github of the project"], ["Example:", "github"]],
+
+    ["The hug command", "Nobody will ever be able to do it", ["\u200b", "\u200b"], ["hug", "\u200b"], ["Argument:", "There is no argument"], ["Explanation:", "Send a text to the weirdo trying to hug the bot"], ["Example:", "hug"]],
+
+    ["The ninjutsu command", "Try to use a ninjutsu on someone", ["\u200b", "\u200b"], ["ninjutsu @someone", "\u200b"], ["Argument:", "You need to mention someone"], ["Explanation:", "The bot will create 2 random numbers. The first one will determine if it was a success or a fail.\nThe second one will detrermine which jutstu will be used."], ["Example:", "ninjutsu @Bluedragon"]],
+
+    ["The pat command", "Headpat someone", ["\u200b", "\u200b"], ["pat @someone", "\u200b"], ["Argument:", "You need to mention someone"], ["Explanation:", "The bot will pat the one you mentioned"], ["Example:", "pat @Bluedragon"]],
+
+    ["The profile command", "To get some general info on a character", ["\u200b", "\u200b"], ["profile name", "\u200b"], ["Argument:", "You need to put a name"], ["Explanation:", "The bot will look if the name you gave is correct or no.\nIf it is, then it will create an embed, with a picture of the character, and some info based on the RPG and the official one."], ["Example:", "profile Enju"]],
+
+    ["The ranking command", "To know how is your girl compared to the others", ["\u200b", "\u200b"], ["ranking name", "\u200b"], ["Argument:", "You need to write the name of a girl"], ["Explanation:", "The bot will compare all the girl.\nThen it will look how the girl is compared to the others.\nTo give the result, the bot will create an embed."], ["Example:", "ranking Akari"]],
+
+    ["The revive command", "Heroes never dies!", ["\u200b", "\u200b"], ["revive @someone", "\u200b"], ["Argument:", "You need to mention someone"], ["Explanation:", "The bot will look if there is someone dead, then it will look if you pinged yourself.\nIf you did, it stop, else it will revive the one you mentioned."], ["Example:", "revive @Bluedragon"]],
+
+    ["The roll command", "Imagine thunderfenril not creating a true gacha command.", ["\u200b", "\u200b"], ["roll category number", "\u200b"], ["Argument:", "You need to mention to select which category you wish to roll, and how much"], ["Explanation:", "The bot will select randomly an item from the category you chose.\nIn case the number is 10, there will be a pity system, where if you don't have an item of rarity 3 or 4, one will be forced for the tenth roll.\nThen the item are sorted randomly."], ["Example:", "roll body 10"]],
+
+    ["The serverinfo command", "Just give a small info on the server", ["\u200b", "\u200b"], ["serverinfo", "\u200b"], ["Argument:", "There is no argument"], ["Explanation:", "Will just says the name of the server and the number of people in it."], ["Example:", "serverinfo"]],
+
+    ["The versus command", "Ready ? Fight", ["\u200b", "\u200b"], ["versus firstFighter secondFighter", "\u200b"], ["Argument:", "You need to write the name of 2 girls."], ["Explanation:", "The bot will compare the 2 fighters trhough their ratings.\nThe winner burst the loser, and show it through an embed with the result of each rounds."], ["Example:", "versus Lily Tengge"]],
+
+    ["The walkthrough command", "I really hate to write this word", ["\u200b", "\u200b"], ["walkthrough [season]", "\u200b"], ["Argument:", "You can input a number to select which season you which to have the walkthrough"], ["Explanation:", "The bot will look if you gave a number or not.\nIf you did and it found it, then it will dm you the link to the guide.\nElse it will create some embed with all the links."], ["Example:", "walktrhough 3"]],
+
+    ["The whip command", "Good way to make someone work", ["\u200b", "\u200b"], ["whip [@someone||text]", "\u200b"], ["Argument:", "You can either mention someone, put a text or just put nothing"], ["Explanation:", "The bot will whip the one you mentioned, what you wrote or everything.\nBe careful to not mention the bot or someone close of the bot."], ["Example:", "whip @Bluedragon"]],
+
+    ["The zap command", "Good way to wake up someone", ["\u200b", "\u200b"], ["zap [@someone||text]", "\u200b"], ["Argument:", "You can either mention someone, put a text or just put nothing"], ["Explanation:", "The bot will zap the one you mentioned, what you wrote or everything.\nBe careful to not mention the bot or someone close of the bot."], ["Example:", "zap @Bluedragon"]],
+
+    ["General info", "\u200B", ["Community category:", "congrats: Aloow to congratulate someone.\nninjutsu: Try to use jutsu on someone.\npat: pat someone.\nrevive: Ressurect someone.\nwhip: Whip someone.\nzap: zap somone."], ["MNG commands:", "birthday: To know a birthday date.\ndata: For a ranking.\nfm: To know if you can beat a stage.\nprofile: To get info on a girl.\nranking: To know how your girl compare to the others.\nversus: To compare 2 girls.\walkthrough: To get a guide."], ["Miscelaneous commands:", "gacha: Holy prayer.\nhug: Pervert.\nroll: let the salt comes."], ["Bot and server command:", "Help: bruh.\nchangelog: latest info on the bot.\ngithub: Link to the project.\nserverinfo: small info on the server."]]
   ]
-  //Redo: gacha, hug, congrats, roll, profile, data, ranking, dm, whip, zap, birthday, ninjutsu, fm, versus
-
-  //To do: github, pat, walkthrough, revive, serverinfo
-
-  //No need: dead, test, sleep
-
-  //Done: changelog
--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const {MessageEmbed} = require('discord.js');
 
+const datahelp=require("../array/dataHelp.js");
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -16,62 +18,76 @@ module.exports = {
     	const embedHelp = new MessageEmbed() //We will create a new embed
     	  embedHelp.setColor(5294200) //We set the color of the embed
     	switch (help) { //We look for the value stored in help
-    	  case "changelog":
-    	    indexHelp=0;
-    	    break;
-    	  case "gacha":
-    	    indexHelp=1
-    	    break;
-    	  case "hug":
-    	    indexHelp=2;
-    	    break;
-    	  case "congrats":
-    	    indexHelp=3;
-    	    break;
-    	  case "roll":
-    	    indexHelp=4;
-    	    break;
-    	  case "profile":
-    	    indexHelp=5;
-    	    break;
-    	  case "data":
-    	    indexHelp=6;
-    	    break;
-    	  case "ranking":
-    	    indexHelp=7
-    	    break;
-    	  case "dm":
-    	    indexHelp=8;
-    	    break;
-    	  case "whip":
-    	  case "zap":
-    	    indexHelp=9;
-    	    break;
-    	  case "birthday":
-    	    indexHelp=10;
-    	    break;
-    	  case "ninjutsu":
-    	    indexHelp = 11;
-    	    break;
-    	  case "fight master":
-    	  case "fm":
-    	    indexHelp = 12 ;
-    	    break;
-    	  case "versus":
-    	    indexHelp = 13;
-    	    break;
-		  case "github":
-			indexHelp=14;
-			break;
+			case "birthday":
+				indexHelp=0;
+				break;
+			case "changelog":
+				indexHelp=1;
+				break;
+			case "congrats":
+				indexHelp=2;
+				break;
+			case "data":
+				indexHelp=3;
+				break;
+			case "fm":
+			case "fight master":
+				indexHelp=4;
+				break;
+			case "gacha":
+				indexHelp=5;
+				break;
+			case "github":
+				indexHelp=6;
+				break;
+			case "hug":
+				indexHelp=7;
+				break;
+			case "ninjutsu":
+				indexHelp=8;
+				break;
+			case "pat":
+				indexHelp=9;
+				break;
+			case "profile":
+				indexHelp=10;
+				break;
+			case "ranking":
+				indexHelp=11;
+				break;
+			case "revive":
+				indexHelp=12;
+				break;
+			case "roll":
+				indexHelp=13;
+				break;
+			case "serverinfo":
+				indexHelp=14;
+				break;
+			case "versus":
+				indexHelp=15;
+				break;
+			case "guide":
+			case "walkthrough":
+				indexHelp=16;
+				break;
+			case "whip":
+				indexHelp=17;
+				break;
+			case "zap":
+				indexHelp=18;
+				break;
+    	  
     	  default:
-    	    indexHelp=dataHelp.length-1;
+    	    indexHelp=datahelp.length-1;
     	}
-    	  embedHelp.setTitle(dataHelp[indexHelp][0])
-    	  embedHelp.setDescription(dataHelp[indexHelp][1])
-    	  embedHelp.addFields({name: dataHelp[indexHelp][2][0], value: dataHelp[indexHelp][2][1]})
-    	  embedHelp.addFields({name: dataHelp[indexHelp][3][0], value: dataHelp[indexHelp][3][1]})
-    	  embedHelp.addFields({name: dataHelp[indexHelp][4][0], value: dataHelp[indexHelp][4][1]})
-    	  embedHelp.addFields({name: dataHelp[indexHelp][5][0], value: dataHelp[indexHelp][5][1]})
-    	message.author.send(embedHelp);
+    	  embedHelp.setTitle(datahelp[indexHelp][0])
+    	  embedHelp.setDescription(datahelp[indexHelp][1])
+    	  embedHelp.addFields({name: datahelp[indexHelp][2][0], value: datahelp[indexHelp][2][1]})
+    	  embedHelp.addFields({name: datahelp[indexHelp][3][0], value: datahelp[indexHelp][3][1]})
+    	  embedHelp.addFields({name: datahelp[indexHelp][4][0], value: datahelp[indexHelp][4][1]})
+    	  embedHelp.addFields({name: datahelp[indexHelp][5][0], value: datahelp[indexHelp][5][1]})
+    	interaction.user.send({embeds: [embedHelp]});
+		return interaction.reply({content:"Help sent", ephemeral: true})
 	},
 };
