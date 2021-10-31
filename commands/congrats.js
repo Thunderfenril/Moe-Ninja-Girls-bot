d08232bd2a346dcda8df96ff3a06bb52921f2986
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-31 15:52:13 +0100

    Updated to take the option in

--- commands/congrats.js
+++ commands/congrats.js
@@ -3,11 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('congrats')
-		.setDescription('Send a string to congrats the person mentionned'),
+		.setDescription('Send a string to congrats the person mentionned')
+    .addUserOption(option =>
+      option.setName("target")
+      .setDescription("The one you wish to congratulate")
+      .setRequired(true)),
 	async execute(interaction) {
-		const taggedUser = message.mentions.users.first();
+		const taggedUser = interaction.options.getUser("target");
     const randomCongrats = Math.round(Math.random()*(100-1)+1);
-    if (taggedUser!==undefined){
+    if (null !== taggedUser){
       if(randomCongrats%2==0){
         return interaction.reply("Congratulation "+"<@"+taggedUser+">"+", I will hire the best cook to celebrate it !").then((msg)=>{
         setTimeout(function(){
@@ -20,6 +24,8 @@ module.exports = {
             msg.edit("D-Don't get any wrong idea about this.\nHentai.");}, 10000)
           })
         }
+      } else {
+        return interaction.reply("You forgot to mention someone");
       }
 	},
 };
