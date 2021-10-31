a1e5021842d6e029e3d20ae4778144e51df7ad1d
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2021-10-31 21:39:37 +0100

    Changed the id

--- commands/sleep.js
+++ commands/sleep.js
@@ -7,8 +7,8 @@ module.exports = {
 		.setDescription('Command to put the bot to sleep'),
 	async execute(interaction) {
 		if (interaction.member.id==botownerid){			
-            interaction.client.channels.cache.get('697617573394776114').reply('Good night everybody');
-            interaction.client.channels.cache.get('697617573394776114').followUp('<:NanaoBlanket:706704532742078557> ')
+            interaction.client.channels.cache.get('879401276671152188').reply('Good night everybody');
+            interaction.client.channels.cache.get('879401276671152188').followUp('<:NanaoBlanket:706704532742078557> ')
 			return process.exit(1)
 	    }
     }
