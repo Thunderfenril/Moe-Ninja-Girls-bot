d1d99317b7764b88f76da3a204de0a30a2f560b4
Nicolas GUYOMARCH <nicolasguyomarch67@gmail.com>
2022-02-07 22:04:10 +0100

    [Updated] rework of the birthday command over.

--- commands/birthday.js
+++ commands/birthday.js
@@ -18,10 +18,12 @@ module.exports = {
   		const day=date.getDate(); //We retrieve the current month as an int from 0 to 11, and we save it in a var
   		const year=date.getFullYear(); //We retrieve the current year and we save it in a var
 		let todayDate = new Date(year, month, day); //Today date
+		const todayDateFixed = new Date(year, month, day)
+		const oneDay = 1000*60*60*24 //Number of millisecond in 1 day
 
 		if(null !== nameTarget) {
 
-			let birthdayCandidate = func.givePositionIndex(args);
+			let birthdayCandidate = func.givePositionIndex(nameTarget);
 
   			if(birthdayCandidate===-1) {return interaction.reply("Are you sure you didn't made a mistake in the name ?")} //In case she hasn't been found
 
@@ -34,44 +36,39 @@ module.exports = {
   		   		girlDate.setFullYear(girlDate.getFullYear() +1) //We increase by one
   			}
 		
-  			let oneDay = 1000*60*60*24 //Number of millisecond in 1 day
-		
   			let numberOfDaysLeft = Math.floor((girlDate.getTime() - todayDate.getTime() )/ oneDay) //Operation for the difference of days between 2 dates
 		
   			return interaction.reply("There is "+numberOfDaysLeft+" days left before "+datainfo[birthdayCandidate][0]+" birthday.")
 		} else {
 
 			let counterArray = 0;
-			let nearBirthday = todayDate.setFullYear(todayDate.getFullYear() +1).getTime();
+			let nearBirthday = todayDate.setFullYear(todayDate.getFullYear() +1);
 			let nearbyBirthdayCharacter;
 			let girldBirthdayDate;
-			let daysLeft;
 			let daysLeftBeforeBirthday;
 
-			for(counterArray; counterArray <datainfo.length(); counterArray++) {
+			for(counterArray; counterArray <datainfo.length; counterArray++) {
 				girldBirthdayDate = new Date(year, datainfo[counterArray][2][1]-1, datainfo[counterArray][2][0]);
 
-				if((datainfo[birthdayCandidate][2][1] == month +1) && (datainfo[birthdayCandidate][2][0] < day)) { //If we are in the month of her birthday, and her birthday is already over
-					girldBirthdayDate.setFullYear(girlDate.getFullYear() +1) //We increase the next birthday year by 1
-			 	} else if (datainfo[birthdayCandidate][2][1] < month+1) { // if we are already past her birthday month
-					girldBirthdayDate.setFullYear(girlDate.getFullYear() +1) //We increase by one
+				if((datainfo[counterArray][2][1] == month +1) && (datainfo[counterArray][2][0] < day)) { //If we are in the month of her birthday, and her birthday is already over
+					girldBirthdayDate.setFullYear(girldBirthdayDate.getFullYear() +1) //We increase the next birthday year by 1
+			 	} else if (datainfo[counterArray][2][1] < month+1) { // if we are already past her birthday month
+					girldBirthdayDate.setFullYear(girldBirthdayDate.getFullYear() +1) //We increase by one
 			 	}
 
 				if(nearBirthday > girldBirthdayDate){
-					nearBirthday = girldBirthdayDate;
+					nearBirthday = girldBirthdayDate.getTime();
 					nearbyBirthdayCharacter = counterArray;
 				}
 			}
-
-			daysLeft = func.daysLeftInMonth(day, month, year); //We calculate how much days is left in this month...
-  		    daysLeftBeforeBirthday = birthdayArray[nearbyBirthdayCharacter][1][0] + daysLeft; //...and we add it to the day of the birthday
+			daysLeftBeforeBirthday = (nearBirthday - todayDateFixed.getTime()) / oneDay
 			
-			if(daysLeftBeforeBirthday === 0){
-				return interaction.reply("Happy birthday "+birthdayArray[nearbyBirthdayCharacter][0]+" !🍰");
+			if(nearBirthday === todayDateFixed.getTime()){
+				return interaction.reply("Happy birthday "+datainfo[nearbyBirthdayCharacter][0]+" !🍰");
 			} else {
-				return interaction.reply("There is "+daysLeftBeforeBirthday+" days before "+birthdayArray[nearbyBirthdayCharacter][0]+" birthday!"); //Message that will be sent, where the numbers of days is saved in each category, and the name is looked in the array
+				return interaction.reply("There is "+daysLeftBeforeBirthday+" days before "+datainfo[nearbyBirthdayCharacter][0]+" birthday!"); //Message that will be sent, where the numbers of days is saved in each category, and the name is looked in the array
 			}
-
+			
 		}
 	},
 };
